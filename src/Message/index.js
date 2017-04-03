1ad8b65147dacbaec8df8435c5c03312eb3fa31f
Brandon Mowat <brandonmowat11@gmail.com>
2017-04-03 12:44:13 -0400

    📚 Update: documentation for message object

--- src/Message/index.js
+++ src/Message/index.js
@@ -3,7 +3,17 @@
 * in rendering messages in the chat feed.
 */
 export default class Message {
-  constructor(id, message, senderName) {
+  /**
+    * Message object for organizing and storing current message data.
+    *
+    * @param {Object} messageData - a JSON object containing all the data for a message.
+    * @param {number} messageData.id - id for grouping messages (0 for blue)
+    * @param {(string|html)} messageData.message - the content of the message to
+    *   be rendered in the bubble
+    * @param {string} [messageData.senderName] - the name of the sender of the
+    *   message
+    */
+  constructor(messageData) {
     this.id = id; // id of the sender (0 is reserved for "blue bubble")
     this.message = message;
     this.senderName = senderName || undefined;
