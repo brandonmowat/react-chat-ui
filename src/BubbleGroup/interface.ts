c0c5dd01d466179f810affbad7946515765a513b
Brandon Mowat <brandonmowat11@gmail.com>
2018-01-04 12:55:37 -0500

    Updates BubbleGroup interface

--- src/BubbleGroup/interface.ts
+++ src/BubbleGroup/interface.ts
@@ -1,7 +1,6 @@
 import { Message, ChatBubble } from '../';
 export default interface BubbleGroupInterface {
   messages: [Message];
-  index: number;
   id: number;
   showSenderName: boolean;
   chatBubble: ChatBubble;
